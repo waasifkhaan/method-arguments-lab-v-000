5e6dcb49b36e20b292a1fdad08634d7a212a8ebd
Wasif Khan <waasif.khaan@gmail.com>
2018-05-04 20:57:22 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  puts "Hi, My name is #{name} & I am an expert at #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
